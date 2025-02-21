a9a45f1b0674a5085167d004e00f234eefddb421
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-21 04:20:08 -0500

    Remove unnecessary variables

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const options = document.querySelector("#options");
-options.addEventListener("click", playRound);
-const scoreBoard = document.querySelector("#scoreBoard");
+const options = document.querySelector('#options');
+options.addEventListener('click', playRound);
 const scores = document.getElementById('scores');
 
 // TRY UNCOUPLING GETCOMPCHOICE FUNC FROM INSIDE THIS LOOP
 function getComputerChoice() {
   // randomly choose 1 option which will serve as the computer's choice
-  const array = ["rock", "paper", "scissors"];
+  const array = ['rock', 'paper', 'scissors'];
   return array[Math.floor(Math.random() * array.length)];
 }
 
@@ -25,43 +24,43 @@ function getPlayerChoice(buttonId) {
 const rock = document.createElement('button');
 rock.setAttribute('name', 'rock');
 rock.setAttribute('id', 'rock');
-rock.addEventListener("click", () => {
+rock.addEventListener('click', () => {
   const rockAudio = new Audio('./audio/rock-audio.wav');
   rockAudio.play();
   getPlayerChoice('rock');
   updateRoundCounter();
 })
 rock.classList.add('player-options-buttons');
-rock.innerHTML = `<image src="/img/rock.png" height="55px"/> `;
+rock.innerHTML = '<image src="/img/rock.png" height="55px"/> ';
 options.appendChild(rock);
 
 const paper = document.createElement('button');
 paper.setAttribute('name', 'paper');
-paper.addEventListener("click", () => {
+paper.addEventListener('click', () => {
   const paperAudio = new Audio('./audio/paper-audio.wav');
   paperAudio.play();
   getPlayerChoice('paper');
   updateRoundCounter();
 })
 paper.classList.add('player-options-buttons');
-paper.innerHTML = `<image src="./img/paper.png" height="55px"/>`;
+paper.innerHTML = '<image src="./img/paper.png" height="55px"/>';
 options.appendChild(paper);
 
 const scissors = document.createElement('button');
 scissors.setAttribute('name', 'scissors');
-scissors.addEventListener("click", () => {
+scissors.addEventListener('click', () => {
   const scissorsAudio = new Audio('./audio/scissors-audio.wav');
   scissorsAudio.play();
   getPlayerChoice('scissors');
   updateRoundCounter();
 })
 scissors.classList.add('player-options-buttons');
-scissors.innerHTML = `<image src="./img/scissors.jpg" height="55px" />`;
+scissors.innerHTML = '<image src="./img/scissors.jpg" height="55px" />';
 options.appendChild(scissors);
 
 // This is where we see which player chooses what
-const turn = document.querySelector("#turn");
-turn.innerHTML = `<p>make your move...</p>`;
+const turn = document.querySelector('#turn');
+turn.innerHTML = '<p>make your move...</p>';
 
 // create variables to keep track of player scores
 let playerScore = 0;
@@ -76,8 +75,8 @@ computerScoreCount.textContent = `Computer: ${computerScore}`;
 scores.appendChild(computerScoreCount);
     
 
-let currentRoundResults = document.querySelector("#current-round-results");
-currentRoundResults.textContent = "And the current round result is..."
+let currentRoundResults = document.querySelector('#current-round-results');
+currentRoundResults.textContent = 'And the current round result is...'
 // currentRoundResults.innerHTML = `<p>And the currentRoundResults is.....</p>`;
 
 function resetScores() {
@@ -108,7 +107,7 @@ function displayGameOverScreen() {
 }
     
 function win() {
-  console.log("WINNER!!!");
+  console.log('WINNER!!!');
   playerScore += 1;
   playerScoreCount.textContent = `Player's score: ${playerScore}`;
   currentRoundResults.innerHTML = `<p> You Win round ${rounds}! </p>`;
@@ -116,7 +115,7 @@ function win() {
 }
 
 function lose() {
-  console.log("LOSER!!!");
+  console.log('LOSER!!!');
   computerScore += 1;
   computerScoreCount.textContent = `Computer's score: ${computerScore}`;
   currentRoundResults.innerHTML = `<p>You LOST round ${rounds}! ):</p>`;
@@ -125,10 +124,9 @@ function lose() {
 
 let playerSelection;
 
-let currentRound;
 let rounds = 1; 
 const roundCounter = document.getElementById('roundcounter');
-roundCounter.innerHTML = `Round: 1`
+roundCounter.innerHTML = 'Round: 1'
 function updateRoundCounter() {
   roundCounter.textContent = `Round: ${rounds}`;
 }
@@ -144,7 +142,7 @@ async function playRound() {
       console.log('THE END');
     } else if (rounds < 3) {
       ++rounds;
-      console.log("play again");
+      console.log('play again');
     }
   };
   loop();
@@ -160,19 +158,19 @@ async function playRound() {
     <p>Player chose ${playerSelection}</p>
     <p>Computer chose ${computerSelection}</p>`;
   if (rounds <= 3) {
-    if ((playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")) {
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')) {
       return win();
-    } else if ((playerSelection === "scissors" && computerSelection === "rock") ||
-    (playerSelection === "rock" && computerSelection === "paper") || 
-    (playerSelection === "paper" && computerSelection === "scissors")) {
+    } else if ((playerSelection === 'scissors' && computerSelection === 'rock') ||
+    (playerSelection === 'rock' && computerSelection === 'paper') || 
+    (playerSelection === 'paper' && computerSelection === 'scissors')) {
       return lose();
     } else {
       currentRoundResults.innerHTML = `
         <p> it's a tie :p </p>
       `;
-      console.log("TIE!");
+      console.log('TIE!');
     }
   };
 }; 
